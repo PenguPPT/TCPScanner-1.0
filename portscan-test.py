f92b2121c5223f79f273454500f5794792052a94
PenguPPT <124870471+PenguPPT@users.noreply.github.com>
2023-02-28 20:58:33 +0800

    Add files via upload

---
+++ portscan-test.py
@@ -0,0 +1,27 @@
+import socket
+import time
+
+host = '192.168.1.4'
+ports = range(1, 1024)
+open_ports = []
+closed_ports = []
+start_time = time.time()
+
+print(f'Target {host}')
+print(f'Ports {ports[0]}-{ports[-1]}')
+
+for port in ports:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.1)
+    try:
+        s.connect((host, port))
+        open_ports.append(port)
+        print(f'Port {port} Open')
+    except (ConnectionRefusedError, socket.timeout):
+        closed_ports.append(port)
+    finally:
+        s.close()
+
+print(f'Open {len(open_ports)}')
+print(f'Close {len(closed_ports)}')
+print(f'Duration {time.time() - start_time} secs')
